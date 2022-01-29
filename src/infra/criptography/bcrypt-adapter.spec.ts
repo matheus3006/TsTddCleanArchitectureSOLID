4f0eacf8fcd32c913119ec0bd6e8adf049ea0b3c
matheus <matheussouza18@live.com>
2022-01-29 10:45:23 -0300

    test: ensure BcryptAdapter Throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -26,4 +26,12 @@ describe('Brcrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  // test('Should return a hash on success', async () => {
+  //   const sut = makeSut()
+  //   // jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(Promise.reject(new Error()))
+
+  //   const promise = sut.encrypt('any_value')
+  //   await expect(promise).rejects.toThrow()
+  // })
 })
